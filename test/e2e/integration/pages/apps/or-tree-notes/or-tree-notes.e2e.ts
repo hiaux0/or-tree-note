c8870c0f0cc76e76e6ac15fd9b42acef2a1bd8a9
hiaux0 <hieudn5293@yahoo.de>
2021-01-10 00:04:37 +0100

    e2e(otn): some more insert/normal cases

--- test/e2e/integration/pages/apps/or-tree-notes/or-tree-notes.e2e.ts
+++ test/e2e/integration/pages/apps/or-tree-notes/or-tree-notes.e2e.ts
@@ -1,6 +1,14 @@
 describe("Aurelia skeleton app", () => {
+  let initialContent: string;
+
   beforeEach(() => {
     cy.visit("#/apps");
+
+    cy.get(".editor-line")
+      .invoke("text")
+      .then((lineTextContent) => {
+        initialContent = lineTextContent;
+      });
   });
 
   it("Should move cursor", () => {
@@ -20,20 +28,29 @@ describe("Aurelia skeleton app", () => {
   });
 
   it("Should write text", () => {
-    let beforeContent: string;
-
-    cy.get(".editor-line")
-      .invoke("text")
-      .then((lineTextContent) => {
-        beforeContent = lineTextContent;
-      });
-
     cy.main().type("i@");
 
     cy.get(".editor-line")
       .invoke("text")
       .then((updatedContent) => {
-        expect(updatedContent).equal(`@${beforeContent}`);
+        expect(updatedContent).equal(`@${initialContent}`);
+      });
+  });
+
+  it.only("Dev: Escape in insert mode should not print escape", () => {
+    cy.main().type("i{esc}");
+    cy.get(".editor-line")
+      .invoke("text")
+      .then((updatedContent) => {
+        expect(updatedContent).equal(initialContent);
+      });
+  });
+  it.skip("Dev: Escape in insert mode, then queue key in normal should not type out", () => {
+    cy.main().type("i{esc}l");
+    cy.get(".editor-line")
+      .invoke("text")
+      .then((updatedContent) => {
+        expect(updatedContent).equal(initialContent);
       });
   });
 });
