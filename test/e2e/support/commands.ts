7b523c0755cc8659e642c7f7106f969a2157557c
hiaux0 <hieudn5293@yahoo.de>
2021-01-09 16:56:44 +0100

    e2e(commands): use '%' like labfolder

--- test/e2e/support/commands.ts
+++ test/e2e/support/commands.ts
@@ -23,3 +23,76 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// tag is '%'
+const markedTestTag = /%([A-Z_a-z-]+)/g;
+const enableLogging = true;
+
+// replace tagging with [data-test=xy]
+export const normalizeSelector = (selector: any): any =>
+  typeof selector === "string"
+    ? selector.replace(markedTestTag, "[data-test=$1]")
+    : selector;
+
+/*
+ * allow tagging data-test attributes with % as selector
+ */
+
+//  https://docs.cypress.io/api/cypress-api/custom-commands.html#Parent-Commands
+["get"].forEach((cmd) => {
+  Cypress.Commands.overwrite(cmd, (fn, selector, ...rest) => {
+    if (enableLogging || rest[0]?.log === true) {
+      return fn(normalizeSelector(selector), ...rest);
+    } else {
+      return fn(normalizeSelector(selector), { ...rest, log: false });
+    }
+  });
+});
+
+// https://docs.cypress.io/api/cypress-api/custom-commands.html#Dual-Commands
+["contains"].forEach((cmd) => {
+  Cypress.Commands.overwrite(cmd, (fn, element, selector, content) => {
+    if (enableLogging) {
+      if (content === undefined) {
+        // cy.contains('Hello')
+        content = selector;
+        return fn(element, content);
+      }
+      // Eg. cy.contains('ul', 'apples')
+      return fn(element, normalizeSelector(selector), content);
+    }
+
+    if (content === undefined) {
+      content = selector;
+      // cy.contains('Hello', {})
+      return fn(element, content, { log: false });
+    }
+    // Eg. cy.contains('ul', 'apples', {})
+    return fn(element, normalizeSelector(selector), content, { log: false });
+  });
+});
+
+// https://docs.cypress.io/api/cypress-api/custom-commands.html#Child-Commands
+[
+  "children",
+  "closest",
+  "filter",
+  "find",
+  "next",
+  "nextAll",
+  "nextUntil",
+  "not",
+  "parent",
+  "parents",
+  "prev",
+  "prevAll",
+  "prevUntil",
+  "siblings",
+].forEach((cmd) => {
+  Cypress.Commands.overwrite(cmd, (fn, element, selector, ...rest) => {
+    if (enableLogging) {
+      return fn(element, normalizeSelector(selector), ...rest);
+    }
+    return fn(element, normalizeSelector(selector), { ...rest, log: false });
+  });
+});
