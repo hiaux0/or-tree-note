94ee27a844a88dce614c49d4021556d1d7ea5305
hiaux0 <hieudn5293@yahoo.de>
2021-01-09 12:55:18 +0100

    chore(cypress.config): change to .ts

--- test/e2e/support/index.ts
+++ test/e2e/support/index.ts
@@ -14,7 +14,9 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands';
+import "./commands";
+
+import "./commands/app";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands');
