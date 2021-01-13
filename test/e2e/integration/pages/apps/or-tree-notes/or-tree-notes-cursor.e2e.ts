89eaaaed2242a59d1a73135952eb8194a85963fb
hiaux0 <hieudn5293@yahoo.de>
2021-01-13 02:40:31 +0100

    e2e(otn): cursor

--- test/e2e/integration/pages/apps/or-tree-notes/or-tree-notes-cursor.e2e.ts
+++ test/e2e/integration/pages/apps/or-tree-notes/or-tree-notes-cursor.e2e.ts
@@ -21,7 +21,7 @@ describe("Aurelia skeleton app", () => {
     });
   });
   const input1 = "eh";
-  it.only(`DEV: ${input1}`, () => {
+  it(`DEV: ${input1}`, () => {
     cy.vim(input1);
     cy.getCssVar("--caret-size-width").then((caretWidth) => {
       cy.get("%caret")
@@ -31,8 +31,18 @@ describe("Aurelia skeleton app", () => {
     });
   });
   const input2 = "ee";
-  it(`DEV: ${input2}`, () => {
+  it.skip(`DEV: ${input2}`, () => {
     cy.vim(input2);
+    cy.getCssVar("--caret-size-width").then((caretWidth) => {
+      cy.get("%caret")
+        .should("exist")
+        .invoke({ timeout: 100 }, "attr", "style")
+        .should("contain", caretWidth * 16);
+    });
+  });
+  const input3 = "eee";
+  it(`DEV: ${input3}`, () => {
+    cy.vim(input3);
     cy.getCssVar("--caret-size-width").then((caretWidth) => {
       cy.get("%caret")
         .should("exist")
