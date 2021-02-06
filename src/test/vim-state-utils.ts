9c3cea182410b4d090a9ffc4397e38bbd330a987
hiaux0 <hieudn5293@yahoo.de>
2021-02-06 13:14:15 +0100

    refac(test.vim): add text as input to `createVimState`

--- src/test/vim-state-utils.ts
+++ src/test/vim-state-utils.ts
@@ -3,9 +3,12 @@ import { Cursor, VimState } from 'modules/vim/vim.types';
 export const VIM_TEST_WHOLEINPUT = ['foo'];
 const initialCursor = { line: 0, col: 0 };
 
-export function createVimState(cursor: Cursor = initialCursor): VimState {
+export function createVimState(
+  text: string = VIM_TEST_WHOLEINPUT[0],
+  cursor: Cursor = initialCursor
+): VimState {
   return {
-    text: VIM_TEST_WHOLEINPUT[0],
+    text,
     cursor,
   };
 }
