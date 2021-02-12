5f43741c302dcc8df29d2f1e6081824cc1836df1
hiaux0 <hieudn5293@yahoo.de>
2021-02-12 18:20:27 +0100

    test(vim.abstract): fix new api

--- test/unit/vim/modes/abstract-mode.spec.ts
+++ test/unit/vim/modes/abstract-mode.spec.ts
@@ -1,45 +1,58 @@
-import { cloneDeep } from "lodash";
-import { tokenizeInput } from "modules/vim/modes/abstract-mode";
-import { Vim } from "modules/vim/vim";
+import { cloneDeep } from 'lodash';
+import { tokenizeInput } from 'modules/vim/modes/abstract-mode';
+import { Vim } from 'modules/vim/vim';
+import { VimMode } from '../../../../src/modules/vim/vim.types';
 
-const input = ["foo"];
+const input = ['foo'];
 const cursor = { line: 0, col: 0 };
 
-describe("Vim - Abstract Mode", () => {
+describe('Vim - Abstract Mode', () => {
   let vim: Vim;
 
   /** *************/
   /** Input Queue */
   /** *************/
 
-  describe("C: Input Queue", () => {
+  describe('C: Input Queue', () => {
     beforeEach(() => {
       vim = new Vim(cloneDeep(input), cloneDeep(cursor));
     });
 
-    it("F: Should execute command in Input Queue", () => {
+    it('F: Should execute command in Input Queue', () => {
       vim.enterInsertMode();
-      const result = vim.queueInput("@");
+      const result = vim.queueInput('@');
       expect(result.vimState.text).toBe(`@${input[0]}`);
     });
-    it("F: Should execute command in Input Queue Sequence", () => {
+    it('F: Should execute command in Input Queue Sequence', () => {
       vim.enterInsertMode();
-      const result = vim.queueInputSequence("345");
+      const result = vim.queueInputSequence('345');
       expect(result).toEqual([
         {
-          vimState: { cursor: { col: 1, line: 0 }, text: "3foo" },
-          targetCommand: "type",
-          lines: ["3foo"],
+          vimState: {
+            cursor: { col: 1, line: 0 },
+            text: '3foo',
+            mode: VimMode.INSERT,
+          },
+          targetCommand: 'type',
+          lines: ['3foo'],
         },
         {
-          vimState: { cursor: { col: 2, line: 0 }, text: "34foo" },
-          targetCommand: "type",
-          lines: ["34foo"],
+          vimState: {
+            cursor: { col: 2, line: 0 },
+            text: '34foo',
+            mode: VimMode.INSERT,
+          },
+          targetCommand: 'type',
+          lines: ['34foo'],
         },
         {
-          vimState: { cursor: { col: 3, line: 0 }, text: "345foo" },
-          targetCommand: "type",
-          lines: ["345foo"],
+          vimState: {
+            cursor: { col: 3, line: 0 },
+            text: '345foo',
+            mode: VimMode.INSERT,
+          },
+          targetCommand: 'type',
+          lines: ['345foo'],
         },
       ]);
     });
@@ -48,14 +61,14 @@ describe("Vim - Abstract Mode", () => {
   /** *************/
   /** Tokenizing */
   /** *************/
-  describe("C: Tokenizing", () => {
-    describe("C: tokenizeInput", () => {
-      it("F: Simple tokenize", () => {
-        const input = "foo bar";
+  describe('C: Tokenizing', () => {
+    describe('C: tokenizeInput', () => {
+      it('F: Simple tokenize', () => {
+        const input = 'foo bar';
         const result = tokenizeInput(input);
         expect(result).toEqual([
-          { end: 2, start: 0, string: "foo", index: 0 },
-          { end: 6, start: 4, string: "bar", index: 1 },
+          { end: 2, start: 0, string: 'foo', index: 0 },
+          { end: 6, start: 4, string: 'bar', index: 1 },
         ]);
       });
     });
