11d7d5b727edf8c2474a89104dc9a1904f403c7b
hdn <hieudn5293@yahoo.de>
2023-06-02 19:39:24 +0200

    feat(modules.array): add split helpers

--- src/modules/array/array-utils.ts
+++ src/modules/array/array-utils.ts
@@ -1,5 +1,11 @@
 import { ArrayType } from 'types/types';
 
+export type ThreeSplitType<T extends unknown[]> = [
+  ArrayType<T>,
+  T,
+  ArrayType<T>
+];
+
 export class ArrayUtils {
   public static findIndexBackwardFromIndex<
     T extends unknown[],
@@ -30,4 +36,54 @@ export class ArrayUtils {
     const result = targetIndex + startIndex + 1; // + 1: account for `newArray` adjustment. It also to fix edge case, where we start on an empty new line
     return result;
   }
+
+  /**
+   * const input = [0, 1, 2, 3, 4, 5, 6];
+   * const result = ArrayUtils.splitFirstMiddleLast(input);
+   * result // [0, [1, 2, 3, 4, 5], 6]
+   */
+  public static splitFirstMiddleLast<T extends unknown[]>(
+    array: T
+  ): T | ThreeSplitType<T> {
+    if (array.length <= 2) return array;
+
+    const split = this.splitIntoParts(array, [1, array.length - 1]);
+    const result = [split[0][0], split[1], split[2][0]];
+    // @ts-ignore
+    return result;
+  }
+
+  public static splitIntoParts<T extends unknown[]>(
+    array: T,
+    splitIndeces: number[]
+  ): T {
+    // @ts-ignore
+    const result: T[] = Array.from(
+      { length: splitIndeces.length + 1 },
+      () => []
+    );
+    let splitTracker = 0;
+    array.forEach((entry, entryIndex) => {
+      const inSplitIndeces = splitIndeces.includes(entryIndex);
+      // const legalIndex = splitTracker + 1 < splitIndeces.length;
+      const legalIndex = true;
+      if (inSplitIndeces && legalIndex) {
+        splitTracker++;
+      }
+
+      result[splitTracker].push(entry);
+    });
+
+    // @ts-ignore
+    return result;
+  }
 }
+
+// const input = [0, 1, 2, 3, 4, 5, 6];
+// const result = ArrayUtils.splitIntoParts(input, [1, 3, 7]);
+// const result = ArrayUtils.splitIntoParts(input, [1, 3]);
+// result; /* ? */
+// const expected = [[0], [1, 2], [3, 4, 5, 6]]; /* ? */
+// ArrayUtils.splitFirstMiddleLast(input); /*?*/
+
+// const asht: ArrayType<string[]>;
