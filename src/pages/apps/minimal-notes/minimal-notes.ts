2b08d4e44b5abc1b60e3b63bd3cbcfc7067f4920
hdn <hieudn5293@yahoo.de>
2023-06-05 01:31:34 +0200

    feat(notes): add active check

--- src/pages/apps/minimal-notes/minimal-notes.ts
+++ src/pages/apps/minimal-notes/minimal-notes.ts
@@ -1,15 +1,21 @@
-import { bindable, bindingMode } from 'aurelia-framework';
+import { bindable } from 'aurelia-framework';
 // import { Logger } from 'common/logging/logging';
 import { initVim } from 'modules/vim/vim-init';
 import { VimStateClass } from 'modules/vim/vim-state';
-import { VimEditorOptionsV2, VimLine, VimMode } from 'modules/vim/vim-types';
+import {
+  VimEditorOptionsV2,
+  VimLine,
+  VimMode,
+  VimStateV2,
+} from 'modules/vim/vim-types';
 import { StorageService } from 'storage/vimStorage';
 import './minimal-notes.scss';
 
 // const logger = new Logger('MinimalNotes');
 
 export class MinimalNotes {
-  @bindable({ defaultBindingMode: bindingMode.fromView }) text: string;
+  @bindable() initialVimState: VimStateV2;
+  @bindable() active = false;
 
   inputContainerRef: HTMLDivElement;
   caretRef: HTMLDivElement;
@@ -24,7 +30,8 @@ export class MinimalNotes {
   }
 
   private async initVim() {
-    const savedVimState = await StorageService.getVimState();
+    const savedVimState =
+      this.initialVimState ?? (await StorageService.getVimState());
     const startLines = savedVimState.lines ?? [];
     this.lines = startLines;
 
@@ -37,6 +44,9 @@ export class MinimalNotes {
       removeTrailingWhitespace: true,
       afterInit: (vim) => {
         this.vimState = vim.vimState;
+      },
+      onBeforeCommand: () => {
+        return this.active;
       },
       commandListener: (vimResult) => {
         if (vimResult.vimState.mode === VimMode.INSERT) {
