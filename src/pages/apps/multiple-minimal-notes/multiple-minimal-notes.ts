454586dc1cf6d81b3a6c54546a2979574dbda7f2
hdn <hieudn5293@yahoo.de>
2023-06-05 01:31:52 +0200

    feat)minimal): add multiple notes

--- src/pages/apps/multiple-minimal-notes/multiple-minimal-notes.ts
+++ src/pages/apps/multiple-minimal-notes/multiple-minimal-notes.ts
@@ -1,6 +1,40 @@
-import {bindable} from 'aurelia-framework';
+import { bindable, computedFrom } from 'aurelia-framework';
 import './multiple-minimal-notes.scss';
+import { VimStateV2 } from 'modules/vim/vim-types';
+import { StorageService } from 'storage/vimStorage';
+import { VimEditorState } from 'store/initial-state';
 
 export class MultipleMinimalNotes {
   @bindable value = 'MultipleMinimalNotes';
+  vimStates: VimStateV2[];
+  vimEditorMap: VimEditorState;
+
+  @computedFrom('vimEditorMap.editors')
+  get vimEditors() {
+    const asArr = Object.values(this.vimEditorMap?.editors ?? {});
+    return asArr;
+  }
+
+  async attached() {
+    this.vimStates = await Promise.all([
+      await StorageService.getVimState(),
+      await StorageService.getVimState(),
+      await StorageService.getVimState(),
+    ]);
+    this.vimStates.forEach((state, index) => (state.id = String(index)));
+
+    this.vimEditorMap = {
+      editors: {
+        '0': {
+          vimState: this.vimStates[0],
+          linesAddons: {},
+        },
+        '1': {
+          vimState: this.vimStates[1],
+          linesAddons: {},
+        },
+      },
+      activeEditorIds: ['0'],
+    };
+  }
 }
