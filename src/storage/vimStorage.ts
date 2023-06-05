9f844421bb64c895ed1b88c892a8a8931683f8e9
hdn <hieudn5293@yahoo.de>
2023-06-05 02:55:11 +0200

    add vimEditor to db

--- src/storage/vimStorage.ts
+++ src/storage/vimStorage.ts
@@ -1,9 +1,23 @@
 import localforage from 'localforage';
 import { VimStateV2 } from 'modules/vim/vim-types';
+import { VimEditorState } from 'store/initial-state';
 
 import { setInDatabase } from './Database';
 
 export class StorageService {
+  public static async saveVimEditors(
+    vimState: VimEditorState
+  ): Promise<VimEditorState> {
+    await setInDatabase('vimEditors', vimState);
+    return vimState;
+  }
+
+  public static async getVimEditors(): Promise<VimEditorState> {
+    let vimState = await localforage.getItem<VimEditorState>('vimEditors');
+    if (!vimState) vimState = {};
+    return vimState;
+  }
+
   public static async saveVimState(vimState: VimStateV2): Promise<VimStateV2> {
     await setInDatabase('vimState', vimState);
     return vimState;
